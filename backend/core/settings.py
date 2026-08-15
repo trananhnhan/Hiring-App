c1ee21c05b6ee03dc8a85be11af15a69cf428117
nhan <trananhnhan4425@gmail.com>
2026-08-15 15:46:24 +0700

    fix security problem

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -143,8 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'trananhnhan4425@gmail.com'
-EMAIL_HOST_PASSWORD = 'wzyw hiuq gqbj dgoo'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
